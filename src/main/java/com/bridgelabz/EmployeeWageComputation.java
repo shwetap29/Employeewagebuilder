ed64836c517bcc2d38ba41d4ac72c673209df708
shwetap29 <shwetaprasad48@gmail.com>
2021-05-21 21:56:32 +0530

    Calculated:Wages For a Month

--- src/main/java/com/bridgelabz/EmployeeWageComputation.java
+++ src/main/java/com/bridgelabz/EmployeeWageComputation.java
@@ -4,40 +4,28 @@ public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage computation");
 
+        int maxWorkingDays=20;
         int ratePerHr = 20;
+        int totalHr =0;
         int empHr = 0, dailyWage;
 
-        double checkEmp =Math.floor(Math.random() * 10) % 3;
-         int c = (int) checkEmp;
+        for (int i=0; i<=maxWorkingDays; i++) {
+            double checkEmp = Math.floor(Math.random() * 10) % 3;
+            int c = (int) checkEmp;
 
-         switch (c) {
-         case 1 :
-               empHr=8;
-               System.out.println("Employee is Present");
-               break;
-          case 2 :
-               empHr=4;
-              System.out.println("Employee is Part Time");
-              break;
-          case 3 :
-                 empHr=0;
-              System.out.println("Employee is Absent");
-              break;
-         }
-
-       // if (checkEmp==Present){
-       //      empHr=8;
-       //      dailyWage = empHr * ratePerHr;
-       //     System.out.println("Employee is Present"+"\n daily Wage is "+dailyWage +"rs");
-       // }
-       // else if (checkEmp==PartTime){
-       //     empHr=4;
-       //     dailyWage = empHr * ratePerHr;
-       //     System.out.println("Part Time Employee's Wage = " + dailyWage);
-       // }
-       // else {
-       //     System.out.println("Employee is Absent"+"\n And hence daily Wage is 0 rs ");
-       // }
+            switch (c) {
+                case 1:
+                    empHr = 8;
+                    break;
+                case 2:
+                    empHr = 4;
+                    break;
+                case 3:
+                    empHr = 0;
+                    break;
+            }
+            totalHr = totalHr + empHr;
+        }
          dailyWage = empHr * ratePerHr;
          System.out.println("daily Wage =" + dailyWage);
 
