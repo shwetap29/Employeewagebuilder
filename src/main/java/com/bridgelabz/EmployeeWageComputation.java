f225d9be2fb879e3879500431a574ef2ca609d80
shwetap29 <shwetaprasad48@gmail.com>
2021-05-21 20:54:19 +0530

    MODIFIED:Calculated Part Time Wage

--- src/main/java/com/bridgelabz/EmployeeWageComputation.java
+++ src/main/java/com/bridgelabz/EmployeeWageComputation.java
@@ -4,14 +4,20 @@ public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage computation");
         int Present = 1;
+        int PartTime = 2;
         int ratePerHr = 20;
         int empHr , dailyWage;
 
-        double checkEmp =Math.floor(Math.random() * 10) % 2;
+        double checkEmp =Math.floor(Math.random() * 10) % 3;
         if (checkEmp==Present){
              empHr=8;
              dailyWage = empHr * ratePerHr;
             System.out.println("Employee is Present"+"\n daily Wage is "+dailyWage +"rs");
+        }
+        else if (checkEmp==PartTime){
+            empHr=4;
+            dailyWage = empHr * ratePerHr;
+            System.out.println("Part Time Employee's Wage = " + dailyWage);
         }
         else {
             System.out.println("Employee is Absent"+"\n And hence daily Wage is 0 rs ");
