2043612e45002af5d29b5510162eaff354fb8988
shwetap29 <shwetaprasad48@gmail.com>
2021-05-21 19:08:09 +0530

    Added: Random function

--- src/main/java/com/bridgelabz/EmployeeWageComputation.java
+++ src/main/java/com/bridgelabz/EmployeeWageComputation.java
@@ -3,5 +3,14 @@ package com.bridgelabz;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage computation");
+        int Present = 1;
+        double checkEmp =Math.floor(Math.random() * 10) % 2;
+        if (checkEmp==Present){
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
+
     }
 }
