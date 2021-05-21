8d576b120733b97b86afde56a7bc40d3db88fb36
shwetap29 <shwetaprasad48@gmail.com>
2021-05-21 19:28:11 +0530

    MODIFIED: Calculated wages for full day

--- src/main/java/com/bridgelabz/EmployeeWageComputation.java
+++ src/main/java/com/bridgelabz/EmployeeWageComputation.java
@@ -4,12 +4,17 @@ public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage computation");
         int Present = 1;
+        int ratePerHr = 20;
+        int empHr , dailyWage;
+
         double checkEmp =Math.floor(Math.random() * 10) % 2;
         if (checkEmp==Present){
-            System.out.println("Employee is Present");
+             empHr=8;
+             dailyWage = empHr * ratePerHr;
+            System.out.println("Employee is Present"+"\n daily Wage is "+dailyWage +"rs");
         }
         else {
-            System.out.println("Employee is Absent");
+            System.out.println("Employee is Absent"+"\n And hence daily Wage is 0 rs ");
         }
 
     }
