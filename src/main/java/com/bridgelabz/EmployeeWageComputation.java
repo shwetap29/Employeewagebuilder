7fe648ba0a07997d36decf83095d7d39087eb651
shwetap29 <shwetaprasad48@gmail.com>
2021-05-21 21:13:18 +0530

    MODIFIED: using switchcase

--- src/main/java/com/bridgelabz/EmployeeWageComputation.java
+++ src/main/java/com/bridgelabz/EmployeeWageComputation.java
@@ -3,25 +3,44 @@ package com.bridgelabz;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage computation");
-        int Present = 1;
-        int PartTime = 2;
+
         int ratePerHr = 20;
-        int empHr , dailyWage;
+        int empHr = 0, dailyWage;
 
         double checkEmp =Math.floor(Math.random() * 10) % 3;
-        if (checkEmp==Present){
-             empHr=8;
-             dailyWage = empHr * ratePerHr;
-            System.out.println("Employee is Present"+"\n daily Wage is "+dailyWage +"rs");
-        }
-        else if (checkEmp==PartTime){
-            empHr=4;
-            dailyWage = empHr * ratePerHr;
-            System.out.println("Part Time Employee's Wage = " + dailyWage);
-        }
-        else {
-            System.out.println("Employee is Absent"+"\n And hence daily Wage is 0 rs ");
-        }
+         int c = (int) checkEmp;
+
+         switch (c) {
+         case 1 :
+               empHr=8;
+               System.out.println("Employee is Present");
+               break;
+          case 2 :
+               empHr=4;
+              System.out.println("Employee is Part Time");
+              break;
+          case 3 :
+                 empHr=0;
+              System.out.println("Employee is Absent");
+              break;
+         }
+
+       // if (checkEmp==Present){
+       //      empHr=8;
+       //      dailyWage = empHr * ratePerHr;
+       //     System.out.println("Employee is Present"+"\n daily Wage is "+dailyWage +"rs");
+       // }
+       // else if (checkEmp==PartTime){
+       //     empHr=4;
+       //     dailyWage = empHr * ratePerHr;
+       //     System.out.println("Part Time Employee's Wage = " + dailyWage);
+       // }
+       // else {
+       //     System.out.println("Employee is Absent"+"\n And hence daily Wage is 0 rs ");
+       // }
+         dailyWage = empHr * ratePerHr;
+         System.out.println("daily Wage =" + dailyWage);
+
 
     }
 }
